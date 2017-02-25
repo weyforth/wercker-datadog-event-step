af63af6bcacf6b12dc7c490728309d1dbd1a0c85
Mike Farrow <contact@mikefarrow.co.uk>
2017-02-25 22:25:17 +0000

    initial changes

--- README.md
+++ README.md
@@ -1,25 +1,19 @@
-# Datadog-event
+# Datadog-notify
 
-Send an event to datadog
+Send a notification event to datadog
 
 ## Options
 
  - `token` (required) Your Datadog Token
- - `title` (required) The title of the event
- - `text`  (required) A description of the event
- - `alert_type` (optional, default: `success`) The type of alert we're generating (`error`, `warning`, `info`, or `success`)
- - `priority` (optional, default: `low`) The priority of the alert we're generating (`low` or `normal`)
+ - `priority` (optional, default: `normal`) The priority of the event we're generating (`low` or `normal`)
 
 ## Example
 
     build:
         after-steps:
-            - datadog-event:
+            - datadog-notify:
                 token: $DATADOG_TOKEN
-                title: MyApp has passed
-                text: MyApp has been marked as passed
-                alert_type: success
-                priority: low
+                priority: normal
 
 ## License
 
--- run.sh
+++ run.sh
@@ -3,33 +3,57 @@ if [ ! -n "$WERCKER_DATADOG_EVENT_TOKEN" ]; then
   exit 1
 fi
 
-if [ ! -n "$WERCKER_DATADOG_EVENT_TITLE" ]; then
-  error 'Please specify title property'
-  exit 1
-fi
-
-if [ ! -n "$WERCKER_DATADOG_EVENT_TEXT" ]; then
-  error 'Please specify text property'
-  exit 1
-fi
-
 if [ ! -n "$WERCKER_DATADOG_EVENT_PRIORITY" ]; then
   error 'Please specify priority property'
   exit 1
 fi
 
-if [ ! -n "$WERCKER_DATADOG_EVENT_ALERT_TYPE" ]; then
-  error 'Please specify alert_type property'
-  exit 1
+if [ "$WERCKER_RESULT" == "passed" ]
+then
+  ALERT_TYPE="success"
+else
+  ALERT_TYPE="error"
 fi
+
+TITLE="$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY"
+
+if [ "$DEPLOY" == "true" ]
+then
+  MESSAGE="[Build]($WERCKER_BUILD_URL)"
+else
+  MESSAGE="[Deploy]($WERCKER_DEPLOY_URL)"
+fi
+
+MESSAGE="$MESSAGE $WERCKER_RESULT"
+
+# message = "[deploy](#{@build.deploy_url}) to #{@build.deploytarget_name}" if @build.is_deploy?
+# message += " for [#{@build.git_owner}/#{@build.git_repo}](#{@build.app_url})"
+# message += " by #{@build.started_by} has #{@build.result}"
 
 
 curl  -X POST -H "Content-type: application/json" \
 -d "{
-      \"title\":            \"$WERCKER_DATADOG_EVENT_TITLE\",
-      \"text\":             \"$WERCKER_DATADOG_EVENT_TEXT\",
+      \"title\":            \"$TITLE\",
+      \"text\":             \"$MESSAGE\",
       \"priority\":         \"$WERCKER_DATADOG_EVENT_PRIORITY\",
-      \"alert_type\":       \"$WERCKER_DATADOG_EVENT_ALERT_TYPE\",
-      \"source_type_name\": \"jenkins\"
+      \"alert_type\":       \"$ALERT_TYPE\",
+      \"tags\": [
+          \"app_name:              $WERCKER_APPLICATION_NAME\",
+      ],
+      \"source_type_name\": \"wercker\"
   }" \
 "https://app.datadoghq.com/api/v1/events?api_key=$WERCKER_DATADOG_EVENT_TOKEN"
+
+#
+# \"build_url:            $WERCKER_APPLICATION_URL\",
+# \"build_url:            $WERCKER_BUILD_URL\",
+# \"run_url:              $WERCKER_RUN_URL\",
+# \"git_owner:            $WERCKER_GIT_OWNER\",
+# \"git_repo:             $WERCKER_GIT_REPOSITORY\",
+# \"git_commit:           $WERCKER_GIT_COMMIT\",
+# \"git_branch:           $WERCKER_GIT_BRANCH\",
+# \"git_domain:           $WERCKER_GIT_DOMAIN\",
+# \"started_by:           $WERCKER_STARTED_BY\",
+# \"result:               $WERCKER_RESULT\",
+# \"deploy_url:           $WERCKER_DEPLOY_URL\",
+# \"deploytarget_name:    $WERCKER_DEPLOYTARGET_NAME\"
